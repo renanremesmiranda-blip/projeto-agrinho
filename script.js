b669b02e3c58a770f6c1df08c7c76f9c877b82da
renanremesmiranda-blip <renan.remes.miranda@escola.pr.gov.br>
2026-06-12 11:05:33 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    document.getElementById("mensagem").innerHTML =
+    "🌱 Cada pequena ação faz diferença! Preserve os animais, reduza o lixo e apoie o agro sustentável.";
+}
